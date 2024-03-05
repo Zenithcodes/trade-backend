3edb2bda99926554cb766d54f0f4fb8f11393ecc
Ishwar chand <zenithcodeslabs@gmail.com>
2024-03-05 23:51:19 +0530

    changes file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
  const DBConnectionString=process.env.DB_Connection_String
  const tokenSecret = process.env.Token_Secret
  const smsConfig ={
-   url:process.env.SMS_Url, 
+   url:process.env.SMS_Url,
    token:process.env.SMS_Token,
    route:process.env.SMS_Route,
    flash:process.env.SMS_Flash
